e09cda4e2c9efe4224c0e5b8ce13fe9eededd909
roseak <rose.kohn@gmail.com>
2016-01-11 09:53:07 -0700

    Add views, links controller, and a before action so that users not logged in are rerouted from the links index.

--- thoughtbox/app/controllers/users_controller.rb
+++ thoughtbox/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :logged_in
+  
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to links_path
     else
       flash[:errors] = @user.errors.full_messages.join(', ')
       render :new
@@ -18,4 +20,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- thoughtbox/config/routes.rb
+++ thoughtbox/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'links#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create]
+  resources :links, only: [:index, :create]
 end
