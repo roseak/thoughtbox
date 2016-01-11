668b796bc25ce4a8660982b8ba7f1c41f4483bd3
roseak <rose.kohn@gmail.com>
2016-01-11 09:24:47 -0700

    User can sign up test and test helper updated.

--- thoughtbox/test/test_helper.rb
+++ thoughtbox/test/test_helper.rb
@@ -1,10 +1,18 @@
+require 'SimpleCov'
+SimpleCov.start('rails')
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
+require 'minitest/unit'
+require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include Capybara::DSL
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+    Capybara.app = Thoughtbox::Application
+  end
 end
